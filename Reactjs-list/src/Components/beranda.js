5c7419559354dcfe5ae00100f1e1f416ba68f93c
Nabila Afifah Qotrunnada <55417374+nabilaafifahqotrunnada@users.noreply.github.com>
2021-05-19 20:15:39 +0700

    Add files via upload

---
+++ Reactjs-list/src/Components/beranda.js
@@ -0,0 +1,13 @@
+import React from "react"
+
+class Beranda extends React.Component{
+    render(){
+        return(
+            <div>
+                <h2>INI WEB HAWDA</h2>
+            </div>
+        )
+    }
+}
+
+export default Beranda;
